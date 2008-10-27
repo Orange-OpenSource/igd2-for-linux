a6b06749b0be6212140380ceb4b4c4cf22f2e7a7
Open Source <opensource@tieto.com>
2008-10-27 13:48:47 +0000

    Added functionalities in SetDomainName,GetDomainName procedures.
    git-svn-id: http://us000253/svn/IGDv2@34 55a18753-f1c3-4722-b33a-e097f9cf6a53

--- linuxigd2/src/lanhostconfig.c
+++ linuxigd2/src/lanhostconfig.c
@@ -22,12 +22,12 @@ int SetDHCPServerConfigurable(struct Upnp_Action_Request *ca_event)
 
     result_str = g_string_new("");
 
-    if( (configurable = GetFirstDocumentItem(ca_event->ActionRequest, "NewDHCPServerConfigurable") ) )
+    if ( (configurable = GetFirstDocumentItem(ca_event->ActionRequest, "NewDHCPServerConfigurable") ) )
     {
         config = resolveBoolean(configurable);
 
         // if user is setting configurable to true, check that init can be run successfully
-        if(config && InitLanHostConfig())
+        if (config && InitLanHostConfig())
         {
             // init failed, send action failed response
             ca_event->ErrCode = 501;
@@ -39,7 +39,7 @@ int SetDHCPServerConfigurable(struct Upnp_Action_Request *ca_event)
             lanHostConfig.DHCPServerConfigurable = config;
             ca_event->ErrCode = UPNP_E_SUCCESS;
             g_string_printf(result_str, "<u:%sResponse xmlns:u=\"%s\">\n%s\n</u:%sResponse>",
-                 ca_event->ActionName, "urn:schemas-upnp-org:service:LANHostConfigManagement:1", "", ca_event->ActionName);
+                            ca_event->ActionName, "urn:schemas-upnp-org:service:LANHostConfigManagement:1", "", ca_event->ActionName);
             ca_event->ActionResult = ixmlParseBuffer(result_str->str);
         }
     }
@@ -51,7 +51,7 @@ int SetDHCPServerConfigurable(struct Upnp_Action_Request *ca_event)
         ca_event->ActionResult = NULL;
     }
 
-    if(configurable) free(configurable);
+    if (configurable) free(configurable);
     g_string_free(result_str, TRUE);
 
     return ca_event->ErrCode;
@@ -65,8 +65,8 @@ int GetDHCPServerConfigurable(struct Upnp_Action_Request *ca_event)
 
     result_str = g_string_new("");
     g_string_printf(result_str, "<u:GetDHCPServerConfigurableResponse xmlns:u=\"urn:schemas-upnp-org:service:LANHostConfigManagement:1\">\n"
-             "<NewDHCPServerConfigurable>%d</NewDHCPServerConfigurable>\n"
-             "</u:GetDHCPServerConfigurableResponse>", (lanHostConfig.DHCPServerConfigurable ? 1 : 0));
+                    "<NewDHCPServerConfigurable>%d</NewDHCPServerConfigurable>\n"
+                    "</u:GetDHCPServerConfigurableResponse>", (lanHostConfig.DHCPServerConfigurable ? 1 : 0));
 
     ParseResponse(ca_event, result_str);
 
@@ -87,6 +87,7 @@ int GetDHCPRelay(struct Upnp_Action_Request *ca_event)
 
 int SetSubnetMask(struct Upnp_Action_Request *ca_event)
 {
+
     return 0;
 }
 
@@ -98,7 +99,7 @@ int GetSubnetMask(struct Upnp_Action_Request *ca_event)
 
     result_str = g_string_new("");
 
-    if(lanHostConfig.DHCPServerConfigurable == FALSE)
+    if (lanHostConfig.DHCPServerConfigurable == FALSE)
     {
         trace(1, "GetSubnetMask: DHCPServerConfigurable is false.");
         addErrorData(ca_event, 501, "Action Failed");
@@ -107,7 +108,7 @@ int GetSubnetMask(struct Upnp_Action_Request *ca_event)
 
     // try to run uci command
     cmd = popen("uci get network.lan.netmask", "r");
-    if(cmd == NULL)
+    if (cmd == NULL)
     {
         trace(1, "GetSubnetMask: getting subnet mask failed.");
         addErrorData(ca_event, 501, "Action Failed");
@@ -118,11 +119,12 @@ int GetSubnetMask(struct Upnp_Action_Request *ca_event)
     fgets(subnet_mask, 48, cmd);
 
     g_string_printf(result_str, "<u:%sResponse xmlns:u=\"urn:schemas-upnp-org:service:LANHostConfigManagement:1\">\n"
-             "<NewSubnetMask>%s</NewSubnetMask>\n"
-             "</u:%sResponse>", ca_event->ActionName, subnet_mask, ca_event->ActionName);
+                    "<NewSubnetMask>%s</NewSubnetMask>\n"
+                    "</u:%sResponse>", ca_event->ActionName, subnet_mask, ca_event->ActionName);
     ParseResponse(ca_event, result_str);
 
     pclose(cmd);
+
     g_string_free(result_str, TRUE);
 
     return 0;
@@ -145,12 +147,91 @@ int GetIPRoutersList(struct Upnp_Action_Request *ca_event)
 
 int SetDomainName(struct Upnp_Action_Request *ca_event)
 {
-    return 0;
+    GString *result_str;
+    FILE *cmd = NULL;
+    char *domainName;
+    char setDname[60];
+
+    if (lanHostConfig.DHCPServerConfigurable == FALSE)
+    {
+        trace(1, "SetDomainName: DHCPServerConfigurable is false.");
+        addErrorData(ca_event, 501, "Action Failed");
+        return ca_event->ErrCode;
+    }
+
+    result_str = g_string_new("");
+
+    if ( (domainName = GetFirstDocumentItem(ca_event->ActionRequest, "NewDomainName") ) )
+    {
+        // try to run uci command
+        snprintf(setDname,60,"uci set dchp.@dnsmasq[0].domain=%s",domainName );
+        cmd = popen(setDname, "r");
+        if (cmd == NULL)
+        {
+            trace(1, "SetDomainName: setting Domain Name failed.");
+            addErrorData(ca_event, 501, "Action Failed");
+            return ca_event->ErrCode;
+        }
+
+        // TODO: to call uci commit function
+
+        ca_event->ErrCode = UPNP_E_SUCCESS;
+        g_string_printf(result_str, "<u:%sResponse xmlns:u=\"%s\">\n%s\n</u:%sResponse>",
+                        ca_event->ActionName, "urn:schemas-upnp-org:service:LANHostConfigManagement:1", "", ca_event->ActionName);
+        ca_event->ActionResult = ixmlParseBuffer(result_str->str);
+
+    }
+    else
+    {
+        trace(1, "Failure in SetDomainName: Invalid Args");
+        ca_event->ErrCode = 402;
+        strcpy(ca_event->ErrStr, "Invalid Args");
+        ca_event->ActionResult = NULL;
+    }
+
+    if(cmd)pclose(cmd);
+    if (domainName) free(domainName);
+    g_string_free(result_str, TRUE);
+
+    return ca_event->ErrCode;
 }
 
 int GetDomainName(struct Upnp_Action_Request *ca_event)
 {
-    return 0;
+    GString *result_str;
+    FILE *cmd;
+    char domain_name[40];
+
+    result_str = g_string_new("");
+
+    if (lanHostConfig.DHCPServerConfigurable == FALSE)
+    {
+        trace(1, "GetDomainName: DHCPServerConfigurable is false.");
+        addErrorData(ca_event, 501, "Action Failed");
+        return ca_event->ErrCode;
+    }
+
+    // try to run uci command
+    cmd = popen("uci get dchp.@dnsmasq[0].domain", "r");
+    if (cmd == NULL)
+    {
+        trace(1, "GetDomainName: getting Domain Name failed.");
+        addErrorData(ca_event, 501, "Action Failed");
+        return ca_event->ErrCode;
+    }
+
+    // get result
+    fgets(domain_name, 40, cmd);
+
+    g_string_printf(result_str, "<u:%sResponse xmlns:u=\"urn:schemas-upnp-org:service:LANHostConfigManagement:1\">\n"
+                    "<NewDomainName>%s</NewDomainName>\n"
+                    "</u:%sResponse>", ca_event->ActionName, domain_name, ca_event->ActionName);
+    ParseResponse(ca_event, result_str);
+
+    pclose(cmd);
+    g_string_free(result_str, TRUE);
+
+    return ca_event->ErrCode;
 }
 
 int SetAddressRange(struct Upnp_Action_Request *ca_event)
@@ -203,16 +284,16 @@ int GetDNSServers(struct Upnp_Action_Request *ca_event)
     regcomp(&nameserver, "nameserver[[:blank:]]*([[:digit:]]{1,3}[.][[:digit:]]{1,3}[.][[:digit:]]{1,3}[.][[:digit:]]{1,3})", REG_EXTENDED);
 
     file = fopen(g_vars.resolvConf, "r");
-    if(file == NULL)
+    if (file == NULL)
     {
         trace(1, "Failed to open resolv.conf at: %s.", g_vars.resolvConf);
         addErrorData(ca_event, 501, "Action Failed");
         return ca_event->ErrCode;
     }
 
-    while(fgets(line, MAX_CONFIG_LINE, file) != NULL)
+    while (fgets(line, MAX_CONFIG_LINE, file) != NULL)
     {
-        if(regexec(&nameserver, line, SUB_MATCH, submatch, 0) == 0)
+        if (regexec(&nameserver, line, SUB_MATCH, submatch, 0) == 0)
         {
             // nameserver found, get it and add to list
             if (strlen(dns_servers->str) > 0)
@@ -224,8 +305,8 @@ int GetDNSServers(struct Upnp_Action_Request *ca_event)
     }
 
     g_string_printf(result_str, "<u:%sResponse xmlns:u=\"urn:schemas-upnp-org:service:LANHostConfigManagement:1\">\n"
-             "<NewDNSServers>%s</NewDNSServers>\n"
-             "</u:%sResponse>", ca_event->ActionName, dns_servers->str, ca_event->ActionName);
+                    "<NewDNSServers>%s</NewDNSServers>\n"
+                    "</u:%sResponse>", ca_event->ActionName, dns_servers->str, ca_event->ActionName);
     ParseResponse(ca_event, result_str);
 
     g_string_free(result_str, TRUE);
@@ -240,14 +321,14 @@ int InitLanHostConfig()
 {
     lanHostConfig.DHCPServerConfigurable = TRUE;
     // check that dnsmasq exists
-    if(!g_file_test(g_vars.dnsmasqCmd, G_FILE_TEST_EXISTS))
+    if (!g_file_test(g_vars.dnsmasqCmd, G_FILE_TEST_EXISTS))
     {
         lanHostConfig.DHCPServerConfigurable = FALSE;
         trace(1, "DHCPServerConfigurable set to false, dnsmasq not found at: %s.", g_vars.dnsmasqCmd);
         return 1;
     }
     // check that uci exists
-    if(!g_file_test(g_vars.uciCmd, G_FILE_TEST_EXISTS))
+    if (!g_file_test(g_vars.uciCmd, G_FILE_TEST_EXISTS))
     {
         lanHostConfig.DHCPServerConfigurable = FALSE;
         trace(1, "DHCPServerConfigurable set to false, uci not found at: %s.", g_vars.uciCmd);
