370dc5832a781a0da790eedb07447bf0f162fe39
Jaakko Pasanen <jaakko.pasanen@tieto.com>
2009-03-25 10:14:58 +0000

    Change applicationlevelsecurity to use wpsutil api functions for input creation. Add more svn:ignores for wpsutil
    git-svn-id: http://us000253/svn/IGDv2@131 55a18753-f1c3-4722-b33a-e097f9cf6a53

--- linuxigd2/src/applicationlevelsecurity.c
+++ linuxigd2/src/applicationlevelsecurity.c
@@ -12,7 +12,7 @@
 WPSuEnrolleeSM* esm;
 unsigned char* Enrollee_send_msg;
 int Enrollee_send_msg_len;
-struct WPSuEnrolleeInput input;
+WPSuStationInput input;
 
 int reactor_loop = 1;
 
@@ -20,6 +20,34 @@ int InitALS()
 {   
     // TODO these values. What those should be and move to config file 
     // TODO: Should start new thread for multiple simultanious registration processes?
+    int err;
+    
+    unsigned char MAC[SC_MAC_LEN];
+    memset(MAC, 0xAB, SC_MAC_LEN);
+
+    err = wpsu_enrollee_station_input_add_device_info(&input, 
+                                        "stasecret",
+                                        "TestManufacturer", 
+                                        "TestModelName",
+                                        "TestModelNumber", 
+                                        "TestSerialNumber", 
+                                        "TestDeviceName", 
+                                        NULL,
+                                        0,
+                                        MAC,
+                                        SC_MAC_LEN,
+                                        "TestUUID",
+                                        8,
+                                        NULL,
+                                        0,
+                                        NULL,
+                                        0,
+                                        SC_CONF_METHOD_LABEL | SC_CONF_METHOD_PUSHBUTTON, 
+                                        SC_RFBAND_2_4GHZ);
+                                        
+                                
+                                        
+    /*
     strcpy(input.OwnInfo.Manufacturer, "TestManufacturer");
     strcpy(input.OwnInfo.ModelName, "TestModelName");
     strcpy(input.OwnInfo.ModelNumber, "TestModelNumber");
@@ -30,34 +58,60 @@ int InitALS()
     input.OwnInfo.ConfigMethods = SC_CONF_METHOD_LABEL | SC_CONF_METHOD_PUSHBUTTON;
     input.OwnInfo.RFBands = SC_RFBAND_2_4GHZ;
     strcpy(input.DevicePIN, "stasecret");
-
+*/
     // station has applications A, B and C
     input.Apps = 3;
 
-    memset(input.AppsList[0].UUID, 0xAA, SC_MAX_UUID_LEN);
-    input.AppsList[0].DataLen = 0;
+unsigned char UUID[SC_MAX_UUID_LEN];
+unsigned char *DATA;
 
-    input.AppsList[1].Data = (unsigned char*) malloc(1000);
-    memset(input.AppsList[1].UUID, 0xBB, SC_MAX_UUID_LEN);
-    strcpy(input.AppsList[1].Data, "B data from STA");
-    input.AppsList[1].DataLen = strlen("B data from STA") + 1;
+    memset(UUID, 0xAA, SC_MAX_UUID_LEN);
+    //input.AppsList[0].DataLen = 0;
 
-    input.AppsList[2].Data = (unsigned char*) malloc(1000);
-    memset(input.AppsList[2].UUID, 0xCC, SC_MAX_UUID_LEN);
-    strcpy(input.AppsList[2].Data, "C data from STA");
-    input.AppsList[2].DataLen = strlen("C data from STA") + 1;
+err =  wpsu_enrollee_station_input_add_app(&input,
+    UUID,SC_MAX_UUID_LEN,
+    NULL,0,
+    NULL,0);
+
+    //input.AppsList[1].Data = (unsigned char*) malloc(1000);
+    //memset(input.AppsList[1].UUID, 0xBB, SC_MAX_UUID_LEN);
+    //strcpy(input.AppsList[1].Data, "B data from STA");
+    //input.AppsList[1].DataLen = strlen("B data from STA") + 1;
+    
+    memset(UUID, 0xBB, SC_MAX_UUID_LEN);
+
+err =  wpsu_enrollee_station_input_add_app(&input,
+    UUID,SC_MAX_UUID_LEN,
+    "B data from STA",strlen("B data from STA") + 1,
+    NULL,0);
+
+    //input.AppsList[2].Data = (unsigned char*) malloc(1000);
+    //memset(input.AppsList[2].UUID, 0xCC, SC_MAX_UUID_LEN);
+    //strcpy(input.AppsList[2].Data, "C data from STA");
+    //input.AppsList[2].DataLen = strlen("C data from STA") + 1;
+
+    memset(UUID, 0xCC, SC_MAX_UUID_LEN);
+    
+
+err =  wpsu_enrollee_station_input_add_app(&input,
+    UUID,SC_MAX_UUID_LEN,
+    "C data from STA",strlen("C data from STA") + 1,
+    NULL,0);
 
     // create enrollee state machine
-    esm = wpsu_create_enrollee_sm_station(&input);
+
+    esm = wpsu_create_enrollee_sm_station(&input, &err);
+    printf ("wpsu_create_enrollee_sm_station %d\n",err);
     
     return 0;
 }
 
 void FreeALS()
 {
+    int error;
     free(input.AppsList[1].Data);
     free(input.AppsList[2].Data);
-    wpsu_cleanup_enrollee_sm(esm);
+    wpsu_cleanup_enrollee_sm(esm, &error);
 }
 
 /**
@@ -73,18 +127,18 @@ int message_received(int error, unsigned char *data, int len, void* control)
         return error;
     }
 
-    wpsu_update_enrollee_sm(esm, data, len, &Enrollee_send_msg, &Enrollee_send_msg_len, &status);
+    wpsu_update_enrollee_sm(esm, data, len, &Enrollee_send_msg, &Enrollee_send_msg_len, &status, &error);
 
     switch (status)
     {
-        case WPSU_E_SUCCESS:
+        case SC_E_SUCCESS:
         {
             trace(3,"Last message received!\n");
             FreeALS();
             reactor_loop = 0;
             break;
         }
-        case WPSU_E_SUCCESSINFO:
+        case SC_E_SUCCESSINFO:
         {
             trace(3,"Last message received M2D!\n");
             FreeALS();
@@ -92,7 +146,7 @@ int message_received(int error, unsigned char *data, int len, void* control)
             break;
         }
 
-        case WPSU_E_FAILURE:
+        case SC_E_FAILURE:
         {
             trace(3,"Error in state machine. Terminating...\n");
             FreeALS();
@@ -100,7 +154,7 @@ int message_received(int error, unsigned char *data, int len, void* control)
             break;
         }
 
-        case WPSU_E_FAILUREEXIT:
+        case SC_E_FAILUREEXIT:
         {
             trace(3,"Error in state machine. Terminating...\n");
             FreeALS();
@@ -124,11 +178,12 @@ int message_received(int error, unsigned char *data, int len, void* control)
 int GetDeviceInfo(struct Upnp_Action_Request *ca_event)
 {
     char resultStr[RESULT_LEN];
+    int error;
     
     // temporarly this is here. This should probably be somewhere else?
     InitALS();
     // start the state machine and create M1
-    wpsu_start_enrollee_sm(esm, &Enrollee_send_msg, &Enrollee_send_msg_len);
+    wpsu_start_enrollee_sm(esm, &Enrollee_send_msg, &Enrollee_send_msg_len, &error);
       
     // to base64   
     int maxb64len=2*Enrollee_send_msg_len; 
