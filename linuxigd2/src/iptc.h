e9e5ad7f98f6113953b712cd5558b087a3bb3605
Jaakko Pasanen <jaakko.pasanen@tieto.com>
2009-04-24 10:16:52 +0000

    Doen't need linux-headers anymore for compiling. Copied some structs from linux-header file to iptc.h. (different linux-header versions caused some problems with compiling)
    git-svn-id: http://us000253/svn/IGDv2@169 55a18753-f1c3-4722-b33a-e097f9cf6a53

--- linuxigd2/src/iptc.h
+++ linuxigd2/src/iptc.h
@@ -3,7 +3,68 @@
 
 #include <iptables.h>
 #include <libiptc/libiptc.h>
-#include <net/netfilter/nf_nat.h>
+
+/* Original LinuxIGD used ip_nat_multi_range struct found from linux/netfilter_ipv4/ip_nat.h
+ * For some reason that header file doesn't exist anymore and structs defined there were
+ * moved to linux headers (.../include/net/netfilter/nf_nat.h). All worked well when that 
+ * file was included from headers for kernel 2.6.24-22, but for 2.6.27-11 it didn't work anymore.
+ * So basically I copied following three unions and structs from that older kernel-header file. 
+ * Someone can do better solution by rewriting some code from iptc.c and here.
+ */
+
+#ifndef IP_NAT_RANGE_MAP_IPS
+#define IP_NAT_RANGE_MAP_IPS 1
+#endif
+#ifndef IP_NAT_RANGE_PROTO_SPECIFIED
+#define IP_NAT_RANGE_PROTO_SPECIFIED 2
+#endif
+#ifndef IP_NAT_RANGE_PROTO_RANDOM
+#define IP_NAT_RANGE_PROTO_RANDOM 4
+#endif
+
+union nf_conntrack_man_proto
+{
+    /* Add other protocols here. */
+    __be16 all;
+
+    struct {
+        __be16 port;
+    } tcp;
+    struct {
+        __be16 port;
+    } udp;
+    struct {
+        __be16 id;
+    } icmp;
+    struct {
+        __be16 port;
+    } sctp;
+    struct {
+        __be16 key; /* GRE key is 32bit, PPtP only uses 16bit */
+    } gre;
+};
+
+/* Single range specification. */
+struct nf_nat_range
+{
+    /* Set to OR of flags above. */
+    unsigned int flags;
+
+    /* Inclusive: network order. */
+    __be32 min_ip, max_ip;
+
+    /* Inclusive: network order */
+    union nf_conntrack_man_proto min, max;
+};
+
+/* For backwards compat: don't use in modern code. */
+struct nf_nat_multi_range_compat
+{
+    unsigned int rangesize; /* Must be 1. */
+
+    /* hangs off end. */
+    struct nf_nat_range range[1];
+};
 
 struct ipt_natinfo
 {
