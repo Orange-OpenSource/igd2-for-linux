892e561eea8cfe811bbd2f583375dfd417a4c76c
Jukka Kinnunen <jukka.kinnunen@tieto.com>
2010-09-20 16:44:03 +0300

    Licensing texts added to the new files. Note: the text in pki.c was misleading, that was checked from the original author (pasanjaa).

--- gupnp/gupnp-tools/src/universal-cp/statusbar.c
+++ gupnp/gupnp-tools/src/universal-cp/statusbar.c
@@ -1,7 +1,19 @@
-/* * statusbar.c
+/**
+ * This file is part of Nokia DeviceProtection v1 reference implementation
+ * Copyright © 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * Contact:mika.saaranen@nokia.com
  *
- *  Created on: May 19, 2009
- *      Author: vlillvis
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
 #include "statusbar.h"
--- gupnp/gupnp-tools/src/universal-cp/user-login-setup-dialog.c
+++ gupnp/gupnp-tools/src/universal-cp/user-login-setup-dialog.c
@@ -1,15 +1,19 @@
-/*
- * user-login-setup-dialog.c
+/**
+ * This file is part of Nokia DeviceProtection v1 reference implementation
+ * Copyright © 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * Contact:mika.saaranen@nokia.com
  *
- *  Created on: Jun 4, 2009
- *      Author: vlillvis
- */
-
-/*
- * user-administration-dialog.c
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 2 of the License.
  *
- *  Created on: Jun 4, 2009
- *      Author: vlillvis
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
 #include <string.h>
