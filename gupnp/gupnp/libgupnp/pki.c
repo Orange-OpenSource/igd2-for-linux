ad5b447589fffc18ade9a85325a1a149d1caf18c
Jukka Kinnunen <jukka.kinnunen@tieto.com>
2010-09-20 16:44:03 +0300

    Licensing texts added to the new files. Note: the text in pki.c was misleading, that was checked from the original author (pasanjaa).

--- gupnp/gupnp/libgupnp/pki.c
+++ gupnp/gupnp/libgupnp/pki.c
@@ -1,20 +1,20 @@
-/* read-file.c -- read file contents into a string
-   Copyright (C) 2006 Free Software Foundation, Inc.
-   Written by Simon Josefsson and Bruno Haible.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation; either version 2.1, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+/**
+ * This file is part of Nokia DeviceProtection v1 reference implementation
+ * Copyright © 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * Contact:mika.saaranen@nokia.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 2.1 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, see http://www.gnu.org/licenses/.
+ */
 
 /************************************************************************
 * Purpose: This file contains functions that operate on X.509 Public-Key 
