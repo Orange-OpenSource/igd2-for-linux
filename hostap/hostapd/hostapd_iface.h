b026107aee12e2796d1c622b6e0365d0e98b1970
Niilo Minkkinen <niilona@gmail.com>
2010-09-29 11:58:34 +0300

    hostapd: introduced function hostapd_push_button-_configuration()

--- hostap/hostapd/hostapd_iface.h
+++ hostap/hostapd/hostapd_iface.h
@@ -76,6 +76,7 @@ int hostapd_update_registrar_state_machine(
 						 int* ready);
 int hostapd_is_authentication_finished( void );
 unsigned char *hostapd_get_uuid_e_ptr( void );
+void hostapd_push_button_configuration( void );
 void hostapd_hmac_sha256(const unsigned char *key, size_t key_len,
 				const unsigned char *data, size_t data_len,
 				unsigned char *mac);
